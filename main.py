c74f8a6fb09a24d377bd4943a6b1c9a084e255de
Anassource <anassource@gmail.com>
2025-11-14 09:58:32 +0530

    scraping a webpage

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+import selectorlib
+
+
+URL = "http://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    """this header tell to the webserver that this is the actually a browser, so its behaving like a browser, because some server don't like script program"""
+def scrape(url):
+    """scrape the page source from the url"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+if __name__ == '__main__':
+    print(scrape(URL))
