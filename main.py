6181c9b1bd659cf9a0cdfe97406778daa3f44f78
Anassource <anassource@gmail.com>
2025-11-18 13:10:34 +0530

    Send Email when New Event is Found on the Website

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
 
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
@@ -20,7 +22,20 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "anas.shaikh7827@gmail.com"
+    password = "bovniwbuqljmesrz"
+
+    receiver = "anas.shaikh7827@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
     print("Email was sent !")
 
 def store(extracted):
@@ -35,9 +50,9 @@ if __name__ == '__main__':
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
-    store(extracted)
 
     content = read(extracted)
     if extracted !="No upcoming tours found":
         if extracted not in content:
-            send_email()
+            store(extracted)
+            send_email(message="Hey, new event is found")
